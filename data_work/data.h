5d5bc912952e94b257467296106c97d98f2074c6
BelovDV <belov.dv@phystech.edu>
2021-03-22 23:08:42 +0300

    non-release rewrite_data

---
+++ data_work/data.h
@@ -0,0 +1,226 @@
+#pragma once
+
+#include<string>
+#include<bitset>
+
+namespace Data
+{
+	const std::string _file_name = "data";
+
+	const int _unit_number_info = 10;
+	const int _unit_number_skills = 2;
+	const int _class_number_skills = 2;
+
+	namespace Race
+	{
+		struct Data
+		{
+			int id;
+			std::string name;
+		};
+	}
+
+	namespace Class
+	{
+		enum Skill
+		{
+			none1,
+			none2
+		};
+
+		struct Transform_passive
+		{
+			int type;
+			int value;
+		};
+
+		struct Transform_active
+		{
+			int type;
+			int value;
+			int cost;
+		};
+
+		struct Data
+		{
+			Transform_active start_active[_unit_number_info];
+			Transform_passive start_passive[_unit_number_info];
+			std::bitset<_class_number_skills> skills;
+			int id;
+			std::string name;
+		};
+	}
+
+	namespace Unit
+	{
+		enum Parameter
+		{
+			lvl,
+			attack,
+			defense,
+
+			damage_min,
+			damage_max,
+			hp,
+
+			speed,
+			initiative,
+
+			shooter,
+			magician,
+		};
+
+		enum Skill
+		{
+			none1,
+			none2
+		};
+
+		struct Info
+		{
+			int parameter[_unit_number_info];
+			std::bitset<_unit_number_skills> skills;
+		};
+
+		struct Data
+		{
+			int id;
+			int cost_A;
+			int cost_B;
+			int cost_C;
+			Info info;
+			std::string race;
+			std::string name;
+		};
+	}
+}
+
+template<typename Stream>
+Stream& operator>>(Stream& in, Data::Unit::Info& info)
+{
+	for (int i = 0; i != Data::_unit_number_info; ++i)
+		in >> info.parameter[i];
+	in >> info.skills;
+	return in;
+}
+
+template<typename Stream>
+Stream& operator>>(Stream& in, Data::Unit::Data& data)
+{
+	in
+		>> data.id
+		>> data.race
+		>> data.name
+		>> data.info
+		>> data.cost_A
+		>> data.cost_B
+		>> data.cost_C;
+	return in;
+}
+
+template<typename Stream>
+Stream& operator>>(Stream& in, Data::Class::Transform_active& data)
+{
+	in
+		>> data.type
+		>> data.value
+		>> data.cost;
+	return in;
+}
+
+template<typename Stream>
+Stream& operator>>(Stream& in, Data::Class::Transform_passive& data)
+{
+	in
+		>> data.type
+		>> data.value;
+	return in;
+}
+
+template<typename Stream>
+Stream& operator>>(Stream& in, Data::Class::Data& data)
+{
+	in
+		>> data.id
+		>> data.name
+		>> data.skills;
+	for (int i = 0; i != Data::_unit_number_info; ++i)
+		in >> data.start_active[i];
+	for (int i = 0; i != Data::_unit_number_info; ++i)
+		in >> data.start_passive[i];
+	return in;
+}
+
+template<typename Stream>
+Stream& operator>>(Stream& in, Data::Race::Data& data)
+{
+	in
+		>> data.id
+		>> data.name;
+	return in;
+}
+
+template<typename Stream>
+Stream& operator<<(Stream& out, const Data::Unit::Info& info)
+{
+	for (int i = 0; i != Data::_unit_number_info; ++i)
+		out << info.parameter[i] << ' ';
+	out << info.skills;
+	return out;
+}
+
+template<typename Stream>
+Stream& operator<<(Stream& out, const Data::Unit::Data& data)
+{
+	out
+		<< data.id << '\t'
+		<< data.race << '\t'
+		<< data.name << '\t'
+		<< data.info << '\t'
+		<< data.cost_A << ' '
+		<< data.cost_B << ' '
+		<< data.cost_C;
+	return out;
+}
+
+template<typename Stream>
+Stream& operator<<(Stream& out, const Data::Class::Transform_active& data)
+{
+	out
+		<< data.type << ' '
+		<< data.value << ' '
+		<< data.cost;
+	return out;
+}
+
+template<typename Stream>
+Stream& operator<<(Stream& out, const Data::Class::Transform_passive& data)
+{
+	out
+		<< data.type << ' '
+		<< data.value;
+	return out;
+}
+
+template<typename Stream>
+Stream& operator<<(Stream& out, const Data::Class::Data& data)
+{
+	out
+		<< data.id << '\t'
+		<< data.name << '\t'
+		<< data.skills << '\t';
+	for (int i = 0; i != Data::_unit_number_info; ++i)
+		out << data.start_active[i] << ' ';
+	for (int i = 0; i != Data::_unit_number_info; ++i)
+		out << data.start_passive[i] << ' ';
+	return out;
+}
+
+template<typename Stream>
+Stream& operator<<(Stream& out, const Data::Race::Data& data)
+{
+	out
+		<< data.id << '\t'
+		<< data.name;
+	return out;
+}
---
+++ data_work/rewrite_data.cpp
@@ -0,0 +1,14 @@
+#include"rewrite.h"
+
+void work()
+{
+	Rewrite rewrite;
+	rewrite.read();
+	rewrite.work();
+}
+
+int main()
+{
+	work();
+	return 0;
+}
