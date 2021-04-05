c2e6a5c2a9ba933e6f44530de456299d45afafc5
BelovDV <belov.dv@phystech.edu>
2021-04-05 22:50:12 +0300

    deleted last version

--- last_attempt/code/AI.h
+++
@@ -1,8 +0,0 @@
-//#pragma once
-//
-//#include"choosing.h"
-//
-//class AI_test : public Chooser
-//{
-//	virtual Field_choice get_AI_choice(const Field& field) { return Field_choice(); }
-//};
--- last_attempt/code/field.cpp
+++
@@ -1,8 +0,0 @@
-#include"field.h"
-
-bool operator<(const Coord& a, const Coord& b)
-{
-	if (a.a != b.a)
-		return a.a < b.a;
-	return a.b < b.b;
-}
